2f1f20d63f835a118cae7706bbd8238ea98c3ddb
imleeesa <lisa.bulegato@itiszuccante.edu.it>
2022-11-02 22:20:46 +0100

    Update punto

--- Punto.java
+++ Punto.java
@@ -1,7 +1,9 @@
 import java.lang.Math;
+import java.util.Scanner;
 public class Punto {
     double x;
     double y;
+    static float re;
     static double z; //static Since you haven't created an object, the non-static method doesn't exist yet. A static method (by definition) always exists.
 
     Punto(double x, double y){
@@ -11,6 +13,7 @@ public class Punto {
 
     public static double Distanza(Punto p1, Punto p2){         
         z = Math.pow((p1.x-p2.x), 2) + Math.pow((p1.y-p2.y), 2);
-        return Math.sqrt(z);
+        re = (float) Math.sqrt(z);
+        return re;
     }
 }
