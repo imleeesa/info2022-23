a30c76e44a810123efd9ac6cb7f817ef669bb9c1
imleeesa <lisa.bulegato@itiszuccante.edu.it>
2022-11-02 22:23:59 +0100

    Update Main + Scelta utente

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Punto p1 = new Punto(2,-5);
-        Punto p2 = new Punto(3,3);
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Inserisci la x del primo punto");
+        double x = in.nextDouble();
+        System.out.println("Inserisci la y del primo punto");
+        double y = in.nextDouble();
+        Punto p1 = new Punto(x,y);
+
+        System.out.println("Inserisci la x del secondo punto");
+        double x2 = in.nextDouble();
+        System.out.println("Inserisci la y del secondo punto");
+        double y2 = in.nextDouble();
+        Punto p2 = new Punto(x2,y2);
 
         System.out.println("Primo punto, x= " + p1.x + " y= " + p1.y);
         System.out.println("Primo punto, x= " + p2.x + " y= " + p2.y);
